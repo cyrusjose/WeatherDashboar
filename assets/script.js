ec750361d15197372110905fd090871727e7cef0
Cyrus Jose <cyrusjose@Cyruss-Mac-mini.local>
2020-06-19 17:32:41 -0700

    slight change

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
-    var weatherKey = "a578b472b89abb8cbd34e73dc5c8b531";
+    var weatherKey = 'a578b472b89abb8cbd34e73dc5c8b531';
 
     // Optional Code for temperature conversion
     var fahrenheit = true;
 
-    $("#convertToCelsius").click(function() {
+    $('#convertToCelsius').click(function() {
         if (fahrenheit) {
-            $("#temperature").text(((($("#temperature").text() - 32) * 5) / 9));
+            $('#temperature').text(((($('#temperature').text() - 32) * 5) / 9));
         }
         fahrenheit = false;
     });
 
-    $("#convertToFahrenheit").click(function() {
+    $('#convertToFahrenheit').click(function() {
         if (fahrenheit == false) {
-            $("#temperature").text((($("#temperature").text() * (9 / 5)) + 32));
+            $('#temperature').text((($('#temperature').text() * (9 / 5)) + 32));
         }
         fahrenheit = true;
     });
