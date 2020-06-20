1077fbaa2fbf3405b947fbf46b639f9b82adb213
Cyrus Jose <cyrusjose@Cyruss-Mac-mini.local>
2020-06-19 19:17:40 -0700

    removed unecessary code

--- assets/script.js
+++ assets/script.js
@@ -21,20 +21,5 @@ $(document).ready(function() {
         $('.windSpeed').text('Wind Speed: ' + wind + ' mph');
     });
 
-    // Optional Code for temperature conversion
-    var fahrenheit = true;
 
-    $('#convertToCelsius').click(function() {
-        if (fahrenheit) {
-            $('#temperature').text(((($('#temperature').text() - 32) * 5) / 9));
-        }
-        fahrenheit = false;
-    });
-
-    $('#convertToFahrenheit').click(function() {
-        if (fahrenheit == false) {
-            $('#temperature').text((($('#temperature').text() * (9 / 5)) + 32));
-        }
-        fahrenheit = true;
-    });
 });
