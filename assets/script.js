5dbb64ef1624701bfb22c494524b21cb43d61bb4
Cyrus Jose <cyrus.jose@outlook.com>
2020-06-22 16:42:21 -0700

    Fixed Local Storage

--- assets/script.js
+++ assets/script.js
@@ -1,102 +1,161 @@
-$(document).ready(function() {
-    $('.btn').on('click', function() {
-        $('.row').removeClass('hide');
-        $('.weather-info').removeClass('hide');
-        var weatherKey = 'a578b472b89abb8cbd34e73dc5c8b531';
-        var cityName = $('.cityName').val();
-        var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + weatherKey;
+$(document).ready(function () {
+ 
+    
 
-        $.ajax({
-            url: queryURL,
-            method: 'GET'
-        }).then(function(response) {
-            console.log(response);
-            // $('.weather-info').text(JSON.stringify(response));
-            var title = response.name;
-            var temp = (response.main.temp * (9 / 5)) - 459.67;
-            var humidity = response.main.humidity;
-            var wind = response.wind.speed;
-            var iconNum = response.weather[0].icon;
-            var iconImage = 'http://openweathermap.org/img/w/' + iconNum + ".png";
-            // var today = response.dt;
-            // var date = new Date(today);
-            // console.log(date.toString())
-            var lat = response.coord.lat;
-            console.log(lat);
-            var lon = response.coord.lon;
-            console.log(lon);
-            // $('.date').text(date.toString());
-            $('.cityText').text(title);
-            $('.todayIcon').attr('src', iconImage);
-            $('.temp').text('Temperature: ' + temp.toFixed(1) + '\u00B0');
-            $('.humidity').text('Humidity: ' + humidity + '\%');
-            $('.windSpeed').text('Wind Speed: ' + wind + ' mph');
+    $(".btn").on("click", function (event) {
+    
+    event.preventDefault();
 
-            var uvURL = 'http://api.openweathermap.org/data/2.5/uvi?appid=' + weatherKey + '&lat=' + lat + '&lon=' + lon;
-            $.ajax({
-                url: uvURL,
-                method: 'Get'
-            }).then(function(UV) {
-                console.log(UV);
-                $('.UV-value').text(UV.value)
-            });
+    var cityList = [];
+    init();
+    function renderCityList(){
+        $('.list-group').empty();
+        for(var i = 0; i < cityList.length; i++){
+            var city = cityList[i];
+            var li = $('<li>');
+            li.text(city);
+            li.attr('class','list-group-item');
+            $('.list-group').append(li);
+        }
+    }
 
-            var fiveDayURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + weatherKey;
+    function init(){
+        var storedCity = JSON.parse(localStorage.getItem('City'));
 
-            $.ajax({
-                url: fiveDayURL,
-                method: 'GET'
-            }).then(function(days) {
-                console.log(days);
-                var dOne = days.list[6];
-                var dTwo = days.list[14];
-                var dThree = days.list[22];
-                var dFour = days.list[23];
-                var dFive = days.list[34];
+        if (storedCity !== null){
+            cityList = storedCity;
+        }
+        renderCityList();
+    }
 
-                var tempOne = (dOne.main.temp * (9/5)) - 459.67;
-                var tempTwo = (dTwo.main.temp * (9/5)) - 459.67;
-                var tempThree = (dThree.main.temp * (9/5)) - 459.67;
-                var tempFour = (dFour.main.temp * (9/5)) - 459.67;
-                var tempFive = (dFive.main.temp * (9/5)) - 459.67;
+    function storeCity(){
+        localStorage.setItem('City', JSON.stringify(cityList));
+    }
+    
+    var cityListText = $('.cityName').val().trim();
 
-                var humOne =dOne.main.humidity; 
-                var humTwo =dTwo.main.humidity; 
-                var humThree =dThree.main.humidity; 
-                var humFour =dFour.main.humidity; 
-                var humFive =dFive.main.humidity; 
+    if(cityListText === ""){
+        return;
+    }
+    
+    cityList.push(cityListText);
+    $('.cityName').empty();
 
-                var iconOne = dOne.weather[0].icon;
-                var iconTwo = dTwo.weather[0].icon;
-                var iconThree = dThree.weather[0].icon;
-                var iconFour = dFour.weather[0].icon;
-                var iconFive = dFive.weather[0].icon;
+    renderCityList();
+    storeCity();
 
-                var icImageOne = 'http://openweathermap.org/img/w/' + iconOne + ".png";
-                var icImageTwo = 'http://openweathermap.org/img/w/' + iconTwo + ".png";
-                var icImageThree = 'http://openweathermap.org/img/w/' + iconThree + ".png";
-                var icImageFour = 'http://openweathermap.org/img/w/' + iconFour + ".png";
-                var icImageFive = 'http://openweathermap.org/img/w/' + iconFive + ".png";
-                
-                $('.tempOne').text('Temperature: ' + tempOne.toFixed(1) + '\u00B0');
-                $('.tempTwo').text('Temperature: ' + tempTwo.toFixed(1) + '\u00B0');
-                $('.tempThree').text('Temperature: ' + tempThree.toFixed(1) + '\u00B0');
-                $('.tempFour').text('Temperature: ' + tempFour.toFixed(1) + '\u00B0');
-                $('.tempFive').text('Temperature: ' + tempFive.toFixed(1) + '\u00B0');
+    $(".row").removeClass("hide");
+    $(".weather-info").removeClass("hide");
+    var weatherKey = "a578b472b89abb8cbd34e73dc5c8b531";
+    var cityName = $(".cityName").val();
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cityName +
+      "&appid=" +
+      weatherKey;
 
-                $('.humOne').text('Humidity: ' +  humOne + '\%');
-                $('.humTwo').text('Humidity: ' +  humTwo + '\%');
-                $('.humThree').text('Humidity: ' +  humThree + '\%');
-                $('.humFour').text('Humidity: ' +  humFour + '\%');
-                $('.humFive').text('Humidity: ' +  humFive + '\%');
-                
-                $('.dOneIcon').attr('src', icImageOne);
-                $('.dTwoIcon').attr('src', icImageTwo);
-                $('.dThreeIcon').attr('src', icImageThree);
-                $('.dFourIcon').attr('src', icImageFour);
-                $('.dFiveIcon').attr('src', icImageFive);
-                
-            });
-        });
-    })
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      
+      var title = response.name;
+      var temp = response.main.temp * (9 / 5) - 459.67;
+      var humidity = response.main.humidity;
+      var wind = response.wind.speed;
+      var iconNum = response.weather[0].icon;
+      var iconImage = "http://openweathermap.org/img/w/" + iconNum + ".png";
+      
+      var lat = response.coord.lat;
+      console.log(lat);
+      var lon = response.coord.lon;
+      console.log(lon);
+      $(".cityText").text(title);
+      $(".todayIcon").attr("src", iconImage);
+      $(".temp").text("Temperature: " + temp.toFixed(1) + "\u00B0");
+      $(".humidity").text("Humidity: " + humidity + "%");
+      $(".windSpeed").text("Wind Speed: " + wind + " mph");
+
+      var uvURL =
+        "http://api.openweathermap.org/data/2.5/uvi?appid=" +
+        weatherKey +
+        "&lat=" +
+        lat +
+        "&lon=" +
+        lon;
+      $.ajax({
+        url: uvURL,
+        method: "Get",
+      }).then(function (UV) {
+        console.log(UV);
+        $(".UV-value").text(UV.value);
+      });
+
+      var fiveDayURL =
+        "http://api.openweathermap.org/data/2.5/forecast?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&appid=" +
+        weatherKey;
+
+      $.ajax({
+        url: fiveDayURL,
+        method: "GET",
+      }).then(function (days) {
+        console.log(days);
+        var dOne = days.list[6];
+        var dTwo = days.list[14];
+        var dThree = days.list[22];
+        var dFour = days.list[23];
+        var dFive = days.list[34];
+
+        var tempOne = dOne.main.temp * (9 / 5) - 459.67;
+        var tempTwo = dTwo.main.temp * (9 / 5) - 459.67;
+        var tempThree = dThree.main.temp * (9 / 5) - 459.67;
+        var tempFour = dFour.main.temp * (9 / 5) - 459.67;
+        var tempFive = dFive.main.temp * (9 / 5) - 459.67;
+
+        var humOne = dOne.main.humidity;
+        var humTwo = dTwo.main.humidity;
+        var humThree = dThree.main.humidity;
+        var humFour = dFour.main.humidity;
+        var humFive = dFive.main.humidity;
+
+        var iconOne = dOne.weather[0].icon;
+        var iconTwo = dTwo.weather[0].icon;
+        var iconThree = dThree.weather[0].icon;
+        var iconFour = dFour.weather[0].icon;
+        var iconFive = dFive.weather[0].icon;
+
+        var icImageOne = "http://openweathermap.org/img/w/" + iconOne + ".png";
+        var icImageTwo = "http://openweathermap.org/img/w/" + iconTwo + ".png";
+        var icImageThree =
+          "http://openweathermap.org/img/w/" + iconThree + ".png";
+        var icImageFour =
+          "http://openweathermap.org/img/w/" + iconFour + ".png";
+        var icImageFive =
+          "http://openweathermap.org/img/w/" + iconFive + ".png";
+
+        $(".tempOne").text("Temperature: " + tempOne.toFixed(1) + "\u00B0");
+        $(".tempTwo").text("Temperature: " + tempTwo.toFixed(1) + "\u00B0");
+        $(".tempThree").text("Temperature: " + tempThree.toFixed(1) + "\u00B0");
+        $(".tempFour").text("Temperature: " + tempFour.toFixed(1) + "\u00B0");
+        $(".tempFive").text("Temperature: " + tempFive.toFixed(1) + "\u00B0");
+
+        $(".humOne").text("Humidity: " + humOne + "%");
+        $(".humTwo").text("Humidity: " + humTwo + "%");
+        $(".humThree").text("Humidity: " + humThree + "%");
+        $(".humFour").text("Humidity: " + humFour + "%");
+        $(".humFive").text("Humidity: " + humFive + "%");
+
+        $(".dOneIcon").attr("src", icImageOne);
+        $(".dTwoIcon").attr("src", icImageTwo);
+        $(".dThreeIcon").attr("src", icImageThree);
+        $(".dFourIcon").attr("src", icImageFour);
+        $(".dFiveIcon").attr("src", icImageFive);
+      });
+    });
+  });
 });
